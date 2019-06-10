60746fa1ffd5a2a79f164aae3bfecafd9058741e
kasselTrankos <alvaro.touzon@gmail.com>
2019-06-10 11:47:33 +0200

    tune if is today

--- src/kalendar.js
+++ src/kalendar.js
@@ -1,10 +1,11 @@
 import moment from 'moment';
 const daysWeek = () => {
+  const isToday = date => Boolean(date.format('YYYY-MM-DD') === moment().format('YYYY-MM-DD'));
   const startOfWeek = moment().startOf('isoWeek');
   const endOfWeek = moment().endOf('isoWeek');
   const start = startOfWeek.format('DD');
   const end = endOfWeek.format('DD');
-  const fillDays = (_,index) => ({date: moment(startOfWeek).add(index, 'days'), isToday: moment(startOfWeek).add(index, 'days') === moment()});
+  const fillDays = (_,index) => ({date: moment(startOfWeek).add(index, 'days'), isToday: isToday(moment(startOfWeek).add(index, 'days'))});
   return Array.from({length: Number(end - start) + 1}, fillDays);
 };
 export const getWeek = () => daysWeek();
--- test/kalendar.spec.js
+++ test/kalendar.spec.js
@@ -8,6 +8,18 @@ describe('Kalendar spects for test', () => {
     const days = getWeek();
     expect(days[0].date.format('DD')).to.be.equal(startOfWeek.format('DD'));
   });
+  it('Only today has property isToday:true', () => {
+    const now = moment();
+    const days = getWeek();
+    days.forEach(({date, isToday})=> {
+      if(now.format('YYYY-MM-DD') === date.format('YYYY-MM-DD')) {
+        expect(isToday).to.be.true;
+      } else {
+        expect(isToday).to.be.false;
+      } 
+
+    });
+  });
   it(`When is no current date given obtains current month name ${moment().format('MMMM')}`, () => {
     const monthName = moment().format('MMMM');
     expect(getMonthName()).to.be.equal(monthName);
