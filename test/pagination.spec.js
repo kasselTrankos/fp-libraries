85b2615f4061250dac4ff180f1dbf83b7f6e179f
kasselTrankos <alvaro.touzon@gmail.com>
2019-07-04 09:19:35 +0200

    add new test for last buttons

--- test/pagination.spec.js
+++ test/pagination.spec.js
@@ -15,46 +15,14 @@ describe('PAGINATION', () => {
     expect(first.text).to.be.equal('1');
   });
   it('expect second page is previous page', () => {
-    const [first, second, ...rest] = getPagination(total)(size)(limit)(4);
-    console.log(rest, '00000');
+    const [first, next] = getPagination(total)(size)(limit)(4);
     expect(first.text).to.be.equal('«');
-    expect(second.text).to.be.equal('‹');
+    expect(next.text).to.be.equal('‹');
+  });
+  it('expect last and previous page boton is', () => {
+    const [last, previous] = getPagination(total)(size)(limit)(4).slice(-2);
+    expect(previous.text).to.be.equal('»');
+    expect(last.text).to.be.equal('›');
   });
 
-  // it('When form the first obtain a objet for first, previous', () => {
-  //   expect(first()).to.eql([{page: 1, text: '«'}]);
-  //   expect(previous(2)).to.eql([{page: 1, text: '‹'}]);
-  //   expect(last(13)).to.eql([{page: 13, text: '»'}]);
-  //   expect(next(13)).to.eql([{page: 14, text: '›'}]);
-  // });
-  // it('When size pagination is bigger than pages, then pagination ons only the pages', ()=>{
-  //   const total = 40;
-  //   const size = 6;
-  //   const limit = 20;
-  //   const page = 1;
-  //   const pages =  getPagination(total)(size)(limit)(page);
-  //   expect(pages).to.deep.equal([ {current: 1, text: '1'}, {current: 2, text: '2'} ]);
-  // });
-  // it('When is at page 2 then show navigation go first and previous', ()=>{
-  //   const total = 190;
-  //   const size = 6;
-  //   const limit = 20;
-  //   const page = 2;
-  //   const pages =  getPagination(total)(size)(limit)(page);
-  //   expect(pages).to.deep.equal([ 
-  //     {text: '«', current: 1}, {text: '‹', current: 1}, {current: 2, text: '2'}, {current: 3, text: '3'}, 
-  //     {current: 4, text: '4'}, {current: 5, text: '5'}, {current: 6, text: '6'}, {current: 7, text: '7'},
-  //     {text: '›', current: 3}, {text: '»', current: 10}]);
-  // });
-  // it('When at the last pages then not show last and next', ()=>{
-  //   const total = 300;
-  //   const size = 6;
-  //   const limit = 20;
-  //   const page = 13;
-  //   const pages =  getPagination(total)(size)(limit)(page);
-  //   expect(pages).to.deep.equal([ 
-  //     {text: '«', current: 1}, {text: '‹', current: 12}, {current: 10, text: '10'}, {current: 11, text: '11'}, 
-  //     {text: '12', current: 12}, {current: 13, text: '13'}, {current: 14, text: '14'}, {current: 15, text: '15'}
-  //     ]);
-  // });
 });
