d4b345720c667a8d6e09c75914077b328478cecb
kasselTrankos <alvaro.touzon@gmail.com>
2019-07-04 20:01:04 +0200

    bvelive for look

--- src/pagination.js
+++ src/pagination.js
@@ -5,7 +5,8 @@ const {Right, Left}  = Either;
 const safePage = cond => cond ? Right({page: 1, text: '«'}) : Left([]);
 const map = fn => right => right.map(fn);
 const value = right => right.value;
-
+const add = value => value + 1; 
+const less = value => value - 1; 
 
 const toArray = x =>[x]; 
 const not = value => !value;
@@ -17,34 +18,29 @@ const getBegin = size => total => total - size;
 const getMaxPages = size => pages => isLessThan(size)(pages) ? pages : size; 
 const getCountPagination = total => size => limit => compose(getMaxPages(size), getSize(total))(limit);
 const getCountPages = total => size => limit => pages => pages < size ? pages : getSize(total)(limit);
-const got = function () { return [...arguments].every(Boolean);}
+const got = cond => cond.every(Boolean);
 const getStart = page => size => total => 
   [isLessThan(total)(size), isAtEndPostion(total)(size)(page)].every(Boolean) ? total - size + 1 : page;
 
 const getPages  = size => start => 
   Array.from({length: size}, (_, i) =>  compose(value, Right, buildPageObject)(start + i));
 
-const firstPage = compose(value, map(toArray), safePage); 
-const previousPage = page => {
-  const previous = page => () => ({page: --page, text:'‹'});
-  return compose(value, map(toArray), map(previous(page)), safePage);
-};
-const lastPage = page => {
-  const last = page => () => ({page, text: '»'});
-  return compose(value, map(toArray), map(last(page)), safePage);
-}
-const nextPage =  page => compose(value, map(toArray), map(x => ({page: ++page, text:'›'})), safePage);
+const lastPage = page =>
+  compose(value, map(toArray), map(x => ({page, text: '»'})), safePage);
+const getPage = page => compose(value, map(toArray), map(page), safePage);
 
 const getPagination = (total = 0) => (size = 6) => (limit = 14) => (page = 1)  =>{
   const count = getCountPagination(total)(size)(limit);
   const pages = getCountPages(total)(size)(limit)(count);
   /// here start the tree decissor
-  return [].concat(
-    compose(firstPage, got)(pages > size, page > 1),
-    compose(previousPage(page), got)(pages > size, page > 1),
+  const overFirst = [page > 1];
+  const pagesOverSize = [pages > size]; 
+  const atEndPages = [compose(not, isAtEndPostion(pages)(size))(page)];
+    compose(getPage(x => ({page: 1, text: '«'})), got)(overFirst),
+    compose(getPage(x => ({page: less(page), text:'‹'})), got)([...overFirst, ...pagesOverSize]),
     compose(getPages(count), getStart(page)(size), getSize(total))(limit), 
-    compose(nextPage(page), got)(pages > size, compose(not, isAtEndPostion(pages)(size))(page)),
-    compose(lastPage(pages), got)(pages > size, compose(not, isAtEndPostion(pages)(size))(page))
+    compose(getPage(x => ({page: add(page), text:'›'})), got)([...pagesOverSize, ...atEndPages]),
+    compose(lastPage(x => ({page: pages, text:'›'})), got)([...pagesOverSize, ...atEndPages])
   );
 };
 
--- test/pagination.spec.js
+++ test/pagination.spec.js
@@ -6,13 +6,15 @@ describe('PAGINATION', () => {
   const total = jsc.integer(200, 230).generator();
   const size  = jsc.integer(2, 4).generator();
   const limit  = jsc.integer(11, 18).generator();
-  it('expect got first page when is over 1 current page', () => {
-    const [first] = getPagination(total)(size)(limit)(2);
+  it('expect got first page when is current page is 2', () => {
+    const [first] = getPagination(total)(size)(limit)(4);
     expect(first.text).to.be.equal('«');
   });
   it('expect dont got first page when is over 1 current page', () => {
-    const [first] = getPagination(total)(size)(limit)(1);
-    expect(first.text).to.be.equal('1');
+    const pages = getPagination(total)(size)(limit)(1);
+    const [first] = pages;
+    console.log(first, pages);
+    expect(first.text).to.not.be.equal('«');
   });
   it('expect second page is previous page', () => {
     const [first, next] = getPagination(total)(size)(limit)(4);
