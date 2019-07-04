f2574d062699fb796a9b00f22939f0c985ea4d89
kasselTrankos <alvaro.touzon@gmail.com>
2019-07-04 21:06:02 +0200

    use getPAge for all

--- src/pagination.js
+++ src/pagination.js
@@ -1,27 +1,29 @@
 import compose from 'lodash/fp/compose';
 import {Either} from 'ramda-fantasy';
-import {lt, add, less, not, toArray, is} from './utils';
+import {lt, add, less, not, toArray, is, prop} from './utils';
 const {Right, Left}  = Either;
 
 
 const map = fn => right => right.map(fn);
 const safePage = cond => cond ? Right({page: 1, text: '«'}) : Left([]);
-const value = right => right.value;
 
 const buildPageObject  = page => ({current: page, text: String(page)});
 const getSize = total => limit => Math.ceil(total / limit);
-const isAtEndPostion  = total => size => page => compose(not, lt(page), getBegin(size))(total);
+const isAtEndPostion  = total => size => page => 
+  compose(not, lt(page), getBegin(size))(total);
 const getBegin = size => total => total - size;
 const getMaxPages = size => pages => lt(pages)(size) ? pages : size; 
-const getCountPagination = total => size => limit => compose(getMaxPages(size), getSize(total))(limit);
+const getCountPagination = total => size => limit => 
+  compose(getMaxPages(size), getSize(total))(limit);
 const getCountPages = total => size => limit => pages => pages < size ? pages : getSize(total)(limit);
 const getStart = page => size => total => 
   is([lt(size)(total), isAtEndPostion(total)(size)(page)]) ? total - size + 1 : page;
 
 const getPages  = size => start => 
-  Array.from({length: size}, (_, i) =>  compose(value, Right, buildPageObject)(start + i));
+  Array.from({length: size}, (_, i) => compose(getPage(x => buildPageObject(add(start)(i))), is)([true]));
+    // compose(prop('value'), Right, buildPageObject)(start + i));
 
-const getPage = page => compose(value, map(toArray), map(page), safePage);
+const getPage = page => compose(prop('value'), map(toArray), map(page), safePage);
 
 const getPagination = (total = 0) => (size = 6) => (limit = 14) => (page = 1)  =>{
   const count = getCountPagination(total)(size)(limit);
@@ -34,7 +36,7 @@ const getPagination = (total = 0) => (size = 6) => (limit = 14) => (page = 1)  =
     compose(getPage(x => ({page: 1, text: '«'})), is)(overFirst),
     compose(getPage(x => ({page: less(page), text:'‹'})), is)([...overFirst, ...pagesOverSize]),
     compose(getPages(count), getStart(page)(size), getSize(total))(limit), 
-    compose(getPage(x => ({page: add(page), text:'›'})), is)([...pagesOverSize, ...atEndPages]),
+    compose(getPage(x => ({page: add(page)(1), text:'›'})), is)([...pagesOverSize, ...atEndPages]),
     compose(getPage(x => ({page: pages, text:'»'})), is)([...pagesOverSize, ...atEndPages])
   );
 };
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,5 @@
-export const value = right => right.value;
 export const prop = prop => obj => obj[prop];
-export const add = value => value + 1; 
+export const add = valueA => valueB => valueA + valueB; 
 export const less = value => value - 1; 
 export const lt = valueA => valueB => valueA < valueB;
 export const toArray = x =>[x]; 
