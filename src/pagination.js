a57ed3fa4b3c90c54831c5f4ff2f41c4c5c4697f
kasselTrankos <alvaro.touzon@gmail.com>
2019-07-04 20:46:31 +0200

    ad utils

--- src/pagination.js
+++ src/pagination.js
@@ -1,47 +1,43 @@
 import compose from 'lodash/fp/compose';
 import {Either} from 'ramda-fantasy';
+import  * as utils from './utils';
 
 const {Right, Left}  = Either;
 
-
 const map = fn => right => right.map(fn);
 const safePage = cond => cond ? Right({page: 1, text: '«'}) : Left([]);
-const value = right => right.value;
-const add = value => value + 1; 
-const less = value => value - 1; 
-const lt = valueA => valueB => valueA < valueB;
-const toArray = x =>[x]; 
-const not = value => !value;
-const is = cond => cond.every(Boolean);
+
 
 const buildPageObject  = page => ({current: page, text: String(page)});
 const getSize = total => limit => Math.ceil(total / limit);
-const isAtEndPostion  = total => size => page => compose(not, lt(page), getBegin(size))(total);
+const isAtEndPostion  = total => size => page => 
+  compose(utils.not, utils.lt(page), getBegin(size))(total);
 const getBegin = size => total => total - size;
-const getMaxPages = size => pages => lt(pages)(size) ? pages : size; 
+const getMaxPages = size => pages => utils.lt(pages)(size) ? pages : size; 
 const getCountPagination = total => size => limit => compose(getMaxPages(size), getSize(total))(limit);
 const getCountPages = total => size => limit => pages => pages < size ? pages : getSize(total)(limit);
 const getStart = page => size => total => 
-  [lt(size)(total), isAtEndPostion(total)(size)(page)].every(Boolean) ? total - size + 1 : page;
+  utils.is([utils.lt(size)(total), isAtEndPostion(total)(size)(page)]) ? total - size + 1 : page;
 
 const getPages  = size => start => 
-  Array.from({length: size}, (_, i) =>  compose(value, Right, buildPageObject)(start + i));
+  Array.from({length: size}, (_, i) =>  compose(utils.value, Right, buildPageObject)(start + i));
 
-const getPage = page => compose(value, map(toArray), map(page), safePage);
+const getPage = page => compose(utils.value, map(utils.toArray), map(page), safePage);
 
 const getPagination = (total = 0) => (size = 6) => (limit = 14) => (page = 1)  =>{
   const count = getCountPagination(total)(size)(limit);
   const pages = getCountPages(total)(size)(limit)(count);
   /// here start the tree decissor
-  const overFirst = [lt(1)(page)];
-  const pagesOverSize = [lt(size)(pages)]; 
-  const atEndPages = [compose(not, isAtEndPostion(pages)(size))(page)];
+  const overFirst = [utils.lt(1)(page)];
+  const pagesOverSize = [utils.lt(size)(pages)]; 
+  const atEndPages = [
+      compose(utils.not, isAtEndPostion(pages)(size))(page)];
   return [].concat(
-    compose(getPage(x => ({page: 1, text: '«'})), is)(overFirst),
-    compose(getPage(x => ({page: less(page), text:'‹'})), is)([...overFirst, ...pagesOverSize]),
+    compose(getPage(x => ({page: 1, text: '«'})), utils.is)(overFirst),
+    compose(getPage(x => ({page: utils.less(page), text:'‹'})), utils.is)([...overFirst, ...pagesOverSize]),
     compose(getPages(count), getStart(page)(size), getSize(total))(limit), 
-    compose(getPage(x => ({page: add(page), text:'›'})), is)([...pagesOverSize, ...atEndPages]),
-    compose(getPage(x => ({page: pages, text:'»'})), is)([...pagesOverSize, ...atEndPages])
+    compose(getPage(x => ({page: utils.add(page), text:'›'})), utils.is)([...pagesOverSize, ...atEndPages]),
+    compose(getPage(x => ({page: pages, text:'»'})), utils.is)([...pagesOverSize, ...atEndPages])
   );
 };
 
---
+++ src/utils/index.js
@@ -0,0 +1,9 @@
+
+
+export const value = right => right.value;
+export const add = value => value + 1; 
+export const less = value => value - 1; 
+export const lt = valueA => valueB => valueA < valueB;
+export const toArray = x =>[x]; 
+export const not = value => !value;
+export const is = cond => cond.every(Boolean);
