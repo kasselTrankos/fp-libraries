5d557016e6e273c477947a19a1878520291b620a
kasselTrankos <alvaro.touzon@gmail.com>
2019-07-04 19:17:40 +0200

    made little reorder get less code

--- src/pagination.js
+++ src/pagination.js
@@ -5,16 +5,6 @@ const {Right, Left}  = Either;
 const safePage = cond => cond ? Right({page: 1, text: '«'}) : Left([]);
 const map = fn => right => right.map(fn);
 const value = right => right.value;
-const truthyEither = c => c ? Either.Right() : Either.Left();
-const fromEither = real => optional => {
-  const g = () => real;
-  const f = () => optional;
-  const either = Either.either(g, f);
-  return f => {
-    const is = compose(either, truthyEither);
-    return cond => compose(is, f)(cond);
-  }
-}
 
 
 const toArray = x =>[x]; 
@@ -27,41 +17,25 @@ const getBegin = size => total => total - size;
 const getMaxPages = size => pages => isLessThan(size)(pages) ? pages : size; 
 const getCountPagination = total => size => limit => compose(getMaxPages(size), getSize(total))(limit);
 const getCountPages = total => size => limit => pages => pages < size ? pages : getSize(total)(limit);
+const got = function () { return [...arguments].every(Boolean);}
 const getStart = page => size => total => 
   [isLessThan(total)(size), isAtEndPostion(total)(size)(page)].every(Boolean) ? total - size + 1 : page;
 
 const getPages  = size => start => 
   Array.from({length: size}, (_, i) =>  compose(value, Right, buildPageObject)(start + i));
 
-const got = function () { return [...arguments].every(Boolean);}
-
-
-
-
-const getFristPages = pages => size => page => {
-  const gotPages = pages => size => page => got(pages > size, page > 1);
-  
-  const Page = compose(safePage, gotPages(pages)(size));
-  const previousPage = compose(value, map(toArray), map(previous(page)), Page);
-  return  [].concat(previousPage(page));
-};
-
-const getFirstPage = compose(value, map(toArray), safePage); 
-const getPreviousPage = page => {
+const firstPage = compose(value, map(toArray), safePage); 
+const previousPage = page => {
   const previous = page => () => ({page: --page, text:'‹'});
   return compose(value, map(toArray), map(previous(page)), safePage);
 };
-
-const getPaginationRight = pages => size => page => {
-  const last = pages => () => ({page: pages, text: '»'});
+const lastPage = page => {
+  const last = page => () => ({page, text: '»'});
+  return compose(value, map(toArray), map(last(page)), safePage);
+}
+const nextPage =  page => {
   const next = page => () => ({page: ++page, text:'›'});
-  const gotPaginationRight = pages => size => page => 
-    got(pages > size, compose(not, isAtEndPostion(pages)(size))(page));
-  
-  const Page = compose(safePage, gotPaginationRight(pages)(size));
-  const lastPage = compose(value, map(toArray), map(last(pages)), Page);
-  const nextPage = compose(value, map(toArray), map(next(pages)), Page);
-  return  [].concat(nextPage(page), lastPage(page));
+  return compose(value, map(toArray), map(next(page)), safePage);
 };
 
 const getPagination = (total = 0) => (size = 6) => (limit = 14) => (page = 1)  =>{
@@ -69,10 +43,11 @@ const getPagination = (total = 0) => (size = 6) => (limit = 14) => (page = 1)  =
   const pages = getCountPages(total)(size)(limit)(count);
   /// here start the tree decissor
   return [].concat(
-    compose(getFirstPage, got)(pages > size, page > 1),
-    compose(getPreviousPage(page), got)(pages > size, page > 1),
+    compose(firstPage, got)(pages > size, page > 1),
+    compose(previousPage(page), got)(pages > size, page > 1),
     compose(getPages(count), getStart(page)(size), getSize(total))(limit), 
-    getPaginationRight(pages)(size)(page)
+    compose(nextPage(page), got)(pages > size, compose(not, isAtEndPostion(pages)(size))(page)),
+    compose(lastPage(pages), got)(pages > size, compose(not, isAtEndPostion(pages)(size))(page))
   );
 };
 
