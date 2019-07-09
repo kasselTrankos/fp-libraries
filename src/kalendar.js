89478dac379ddff26039ab145fd9abe08874256c
kasselTrankos <alvaro.touzon@gmail.com>
2019-07-09 20:39:51 +0200

    stable and little cleanded

--- src/kalendar.js
+++ src/kalendar.js
@@ -12,7 +12,8 @@ const Midnight = ToDate(x=> new Date(x))
   .contramap(tz).contramap(midnight).contramap(clone);
 const isSame = Equivalence((x, y) => x === y)
   .contramap(cast).contramap(Midnight.f).contramap(clone);
-const isLower = Equivalence((x, y) =>  lt(x)(y));
+const isLower = Equivalence((x, y) => lt(x)(y))
+  .contramap(cast).contramap(tz).contramap(clone);
 
 
 
@@ -20,35 +21,27 @@ const startWeek = (date = new Date()) =>
   Midnight.f(compose(moveToDate(date), getStartWeek)(date));
 
 const getStartWeek = date => date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1);
-
-
-
 const fillDays = (current = new Date()) => (_,index) => {
   const date = compose(compose(Midnight.f, plusDays(index)), clone)(current);
   const isToday = isSame.f(date, new Date())
   return { date, isToday }
 };
-
 const getDaysFrom = (length = 7) => (current = new Date()) =>
   Array.from({length}, fillDays(current));
 
+const diffDays = dateA => dateB => {
+  const startDate = compose(substract, cast)(dateB);
+  return compose(toDay, compose(startDate, cast))(dateA);
+};
 
 
-
-export const isBeforeNow  = date => isLower.contramap(cast).contramap(tz)
-.f(getDate(date), new Date());
+export const isBeforeNow  = date => isLower.f(getDate(date), new Date());
 export const getNextWeek = (date = new Date()) => compose(getWeek, plusDays(7))(date);
 export const getPrevWeek = (date = new Date()) => compose(getWeek, plusDays(-7))(date);
 export const getMonthName =  (date = new Date()) => months.es[date.getMonth()];
 export const getWeek = (date = new Date()) => compose(getDaysFrom(7), startWeek)(date);
-export const diffDays = dateA => dateB => {
-  const initDate = compose(substract, cast)(dateB);
-  return compose(toDay, compose(initDate, cast))(dateA);
-};
 
 export const getDaysBetween = dateA => dateB => {
-  const getDays = compose(getDaysFrom, diffDays(dateA))(dateB);
-  return compose(getDays, Midnight.f)(dateA);
+  const days = compose(getDaysFrom, diffDays(dateA))(dateB);
+  return days(dateA);
 }
-
-//compose(getDays, compose(less(cast(dateB)), cast))(dateA);
