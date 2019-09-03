200d7f4ec21165a907d2b79eb859e5c685e8391b
kasselTrankos <alvaro.touzon@gmail.com>
2019-09-03 23:13:13 +0200

    repair testy

--- packages/pagination/index.js
+++ packages/pagination/index.js
@@ -10,21 +10,13 @@ const Any = page => ({
 Any.empty = () => Any([]);
 const min = max => value => Math.min(max, value);
 const getSize = total => limit => Math.ceil(total / limit);
-const _getPages = length => current =>
+const getPages = length => current =>
   Array.from({length}, (_, i) => Page(current - i, String(current - i))).reverse();
 
-<<<<<<< HEAD
-=======
-// get pages of component given necessary values
-const getPages = length => current => 
-  Array.from({length}, (_, i) => compose(getPage(x => buildPageObject(add(current)(i))), is)([true]));
-
-const getPage = page => compose(prop('value'), map(toArray), map(page), safePage);
->>>>>>> 6d6bc47eb8df9d3acff881d52e8dfb659158c6f3
 
 const getPagination = (total = 0) => (size = 6) => (limit = 14) => (page = 1) => {
   const end = compose(min(page + size), getSize(total))(limit);
-  const pages = compose(_getPages(size), less(end))(1);
+  const pages = compose(getPages(size), less(end))(1);
   const isOverFirstPage = lt(1)(page);
   const isLessLastPage = compose(lt(page),  getSize(total))(limit);
 
