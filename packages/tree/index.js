3b9da6e496e0a8f293a2d3d7f37067a7c4bb97fe
kasselTrankos <alvaro.touzon@gmail.com>
2019-09-05 15:19:31 +0200

    done, great exercise, undertand fold an monoid

--- packages/tree/index.js
+++ packages/tree/index.js
@@ -4,6 +4,9 @@ function Leaf(val, ann) {
     val: val,
     toString: () => `Leaf(${val}, ${ann})`,
     map: f => Leaf(val, f(ann)),
+    extend: f => Leaf(val, f(Leaf(val, ann))),
+    changed: ann,
+    reduce: (f, acc) => f(acc, ann),
   };
 }
 // Branch : Tree val ann -> Tree val ann -> ann -> Tree val ann
@@ -14,14 +17,39 @@ function Branch(left, right, ann) {
     right: right,
     toString: () => `Branch(${left}, ${right}, ${ann})`,
     map: f => Branch(left.map(f), right.map(f), f(ann)),
+    extend: f => Branch(left.extend(f), right.extend(f), f(Branch(left, right, ann))),
+    changed: ann || left.changed || right.changed,
+    reduce: (f, acc) => right.reduce(f, left.reduce(f, f(acc, ann))),
   };
 }
 
-const l1 = Leaf(1, 'wat');
-const l2 = Leaf(2, 'yup');
-const b1 = Branch(l1, l2, 'nope');
+// Any : Bool -> Any
+function Any(bool) {
+  return {
+    bool: bool,
+    concat: a => Any(bool || a.bool),
+  };
+}
+Any.empty = () => Any(false);
+const fold = (Monoid, Foldable) =>
+  Foldable.reduce((acc, x) => acc.concat(x), Monoid.empty());
 
+const foldMap = (Monoid, f, Foldable) =>
+  Foldable.reduce((acc, x) => acc.concat(f(x)), Monoid.empty());
 
-console.log(l1.toString(),
-l2.toString(),
-b1.toString());
+const changed = tree => foldMap(Any, Any, tree).bool;
+// fold(Any, tree.map(Any)).bool;
+//tree.reduce((acc, x) => acc || x, false);
+
+const l1 = Leaf(1, false);
+const l2 = Leaf(2, true);
+const l3 = Leaf(3, false);
+const l4 = Leaf(4, false);
+const l5 = Leaf(5, false);
+const b1 = Branch(l1, l2, false);
+const b2 = Branch(l3, l4, false);
+const b3 = Branch(b2, l5, false);
+const b4 = Branch(b2, b1, false);
+const b5 = b4.extend(changed);
+
+console.log( b5.toString());
