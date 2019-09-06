f5873ff659c5a382a2817746bd9ea55bee106a3f
kasselTrankos <alvaro.touzon@gmail.com>
2019-09-06 11:12:33 +0200

    made a contramap need to implement of Applicative

--- packages/kalendar/test/kalendar.spec.js
+++ packages/kalendar/test/kalendar.spec.js
@@ -1,5 +1,5 @@
 import {addDays, getWeek} from './../kalendar';
-import moment from 'moment';
+import moment from 'moment-timezone';
 import {expect} from 'chai';
 import jsc from 'jsverify';
 
@@ -7,23 +7,27 @@ describe('KALENDAR', () => {
   let d;
   before(() => {
     const now =  new Date();
-    const from = new Date(now.setDate(now.getDate()-jsc.integer(0, 100).generator()))
-    const to = new Date(now.setDate(now.getDate()+jsc.integer(0, 100).generator()))
+    const from = new Date(now.setDate(now.getDate()-jsc.integer(0, 100).generator()));
+    const to = new Date(now.setDate(now.getDate()+jsc.integer(0, 100).generator()));
     d = jsc.datetime(from, to).generator();
 
   });
   it(`Add Dayss is working fine`, () => {
-    const add = jsc.nat(365).generator();
-    const tomorrow = addDays(d)(add);
-    const expected  = moment(d).add(add, 'days').format('DD/MM/YYYY');
-    expect(tomorrow.format()).to.equal(expected);
+    const days = jsc.nat(365).generator();
+    const tomorrow = addDays(d)(days);
+    const expected  = moment(d).add(days, 'days');
+    expect(tomorrow.format()).to.equal(expected.format('DD/MM/YYYY HH:mm:ss'));
   });
-  it(`remove xx Dayss is working fine`, () => {
-    const add = jsc.nat(365).generator();
-    const tomorrow = addDays(d)(add * -1);
-    const expected  = moment(d).subtract(add, 'days').format('DD/MM/YYYY');
-    console.log(d, getWeek(d));
-    expect(tomorrow.format()).to.equal(expected);
+  it('get week, ', () => {
+    // const week = getWeek(d);
+
+    // console.log(week, d, moment(d).startOf('week').add(1, 'days').format('DD/MM/YYYY HH:mm:ss'), new Date());
   });
+  // it(`remove xx Dayss is working fine`, () => {
+  //   const add = jsc.nat(365).generator();
+  //   const tomorrow = addDays(d)(add * -1);
+  //   const expected  = moment(d).subtract(add, 'days').format('DD/MM/YYYY');
+  //   expect(tomorrow.format()).to.equal(expected);
+  // });
 
 });
