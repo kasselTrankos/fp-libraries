646b07ea6fd897c6cf5d54c6c9de364291c32237
kasselTrankos <alvaro.touzon@gmail.com>
2019-09-06 12:55:51 +0200

    use timezone only when return the Date, this seems inmutability for readly only

--- packages/kalendar/test/kalendar.spec.js
+++ packages/kalendar/test/kalendar.spec.js
@@ -1,5 +1,5 @@
 import {addDays, getWeek} from './../kalendar';
-import moment from 'moment-timezone';
+import moment from 'moment';
 import {expect} from 'chai';
 import jsc from 'jsverify';
 
@@ -13,15 +13,21 @@ describe('KALENDAR', () => {
 
   });
   it(`Add Dayss is working fine`, () => {
-    const days = jsc.nat(365).generator();
-    const tomorrow = addDays(d)(days);
+    const m = new Date(2019, 8, 10,22,46,57);
+    d = m;
+    const days = 6;//jsc.nat(365).generator();
+    const result = addDays(d)(days);
     const expected  = moment(d).add(days, 'days');
-    expect(tomorrow.format()).to.equal(expected.format('DD/MM/YYYY HH:mm:ss'));
+    const error = `origin: ${d} add ${days}: expected(${expected.toDate()}) but the result is ${result.value}`;
+    expect(result.equals(expected.toDate()), error).to.be.true;
   });
   it('get week, ', () => {
-    // const week = getWeek(d);
+    const result = getWeek(d);
+    const expected = moment(d).startOf('week').add(1, 'days');
+    // console.log(week.format(), expected, d);
+    const error = `origin: ${d} then expected(${expected.toDate()}) but the result is ${result.value}`;
 
-    // console.log(week, d, moment(d).startOf('week').add(1, 'days').format('DD/MM/YYYY HH:mm:ss'), new Date());
+    expect(result.equals(expected.toDate()), error).to.be.true;
   });
   // it(`remove xx Dayss is working fine`, () => {
   //   const add = jsc.nat(365).generator();
