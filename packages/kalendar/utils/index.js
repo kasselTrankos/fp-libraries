ffaa62808b0f175d3e20764bdc730e22f8f65237
kasselTrankos <alvaro.touzon@gmail.com>
2019-09-10 09:55:01 +0200

    made geek and monday

--- packages/kalendar/utils/index.js
+++ packages/kalendar/utils/index.js
@@ -2,7 +2,7 @@ export const compose = (...fncs) => x => fncs.reduce((acc, f) => f(acc), x);
 export const prop = prop => obj => obj[prop];
 export const add = valueA => valueB => valueA + valueB; 
 export const less = value => value - 1; 
-export const substract = valueA => valueB => valueA - valueB; 
+export const subtract = valueA => valueB => valueA - valueB; 
 export const lt = valueA => valueB => valueA < valueB;
 export const toArray = x =>[x]; 
 export const not = value => !value;
