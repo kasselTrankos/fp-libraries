c074b3f701730ab714cc252e1ee10e95424853b4
kasselTrankos <alvaro.touzon@gmail.com>
2019-09-11 22:10:25 +0200

    modify hierachy

--- hierarchy.js
+++ hierarchy.js
@@ -2,3 +2,22 @@ const Any = (value) => ({
   empty: false,
   concat: () => value || false
 });
+
+
+const Roles = daggy.taggedSum('Roles', {
+  Parent: ['id'],
+  Children: ['items'],
+  Nil: []
+});
+
+Roles.prototype.map = function(f) {
+  return this.cata({
+    Parent: id => Roles.Parent(f(id)),
+    Children: items => Roles.Children(f(items)),
+    None: () => Roles.Nil,
+  });
+}
+
+module.exports = {Any, Roles};
+
+
