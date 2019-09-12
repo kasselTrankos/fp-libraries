6e2fea774ce7a88f5e313402a4581261e252d13f
kasselTrankos <alvaro.touzon@gmail.com>
2019-09-12 11:28:13 +0200

    fear(merge) hierarchy

--- packages/hierarchy/hierarchy.js
+++ packages/hierarchy/hierarchy.js
@@ -1,1 +1,24 @@
-const init = true;
+import {taggedSum} from 'daggy';
+const Any = (value) => ({
+  empty: false,
+  concat: () => value || false
+});
+
+
+const Roles = taggedSum('Roles', {
+  Parent: ['id'],
+  Children: ['items'],
+  Nil: []
+});
+
+Roles.prototype.map = function(f) {
+  return this.cata({
+    Parent: id => Roles.Parent(f(id)),
+    Children: items => Roles.Children(f(items)),
+    None: () => Roles.Nil,
+  });
+}
+
+module.exports = {Any, Roles};
+
+
